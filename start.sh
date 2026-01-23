2f5028fc6957894c83890a947151ea3056869a96
bibiki <nberani@hotmail.com>
2026-01-23 12:11:34 +0100

    updating site 1769166694

--- start.sh
+++ start.sh
@@ -3,10 +3,12 @@
 # this script assumes git is installed in the system
 # this script assumes that the user that runs the script has 
 # a github user that has write permission in the github repository where this project resides
-#cd spider
+cd spider
+python3 spider.py
+cd ..
+echo "^^^^"
 pwd
-#python3 spider.py
-#cd ..
+echo "^^^^"
 git status
 git add .
 git commit -m "updating site ${EPOCHSECONDS}"
