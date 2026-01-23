1399da7fe84b731432b7d45fb4378eb0ef71f2c3
bibiki <nberani@hotmail.com>
2026-01-23 12:07:39 +0100

    updating site 1769166459

---
+++ start.sh
@@ -0,0 +1,13 @@
+# this script assumes it is running in the root directory of the project
+# this script assumes python3 and beauutifulspoup4 are installed in the system
+# this script assumes git is installed in the system
+# this script assumes that the user that runs the script has 
+# a github user that has write permission in the github repository where this project resides
+#cd spider
+pwd
+#python3 spider.py
+#cd ..
+git status
+git add .
+git commit -m "updating site ${EPOCHSECONDS}"
+git push
