6bea59c40d906a006afe9ba06ca645661cbb8a4e
bibiki <nberani@hotmail.com>
2026-01-23 12:38:32 +0100

    updating site 1769168312

--- spider/spider.py
+++ spider/spider.py
@@ -11,40 +11,51 @@ headers = {
 }
 
 result = []
-
-print("********************")
-#----------manx.news
-url = "https://www.manx.news/"
-req = requests.get(url, headers)
-soup = BeautifulSoup(html.unescape(req.text), "html.parser")
-
-for e in soup.select("h3.entry-title a"):
-    if "javascript" not in e['href']:
-        result.append(e['href'])
-#        print(e)
-#------------------
-print("********************")
-#---------iomtotday.co.io
-url = "https://www.iomtoday.co.im/news"
-req = requests.get(url, headers)
-soup = BeautifulSoup(html.unescape(req.text), "html.parser")
-for e in soup.select("div div div div div.inner_wrapper div a"):
-    if 'class' not in e and "news" in e['href'] and e['href'][-1] in "0123456789":
-        result.append("https://iomtoday.co.im" + e['href'])
-#        print("https://iomtoday.co.im" + e['href'])
-#------------------------
-print("********************")
-#-----------------gef-im
-url = "https://gef.im/"
-req = requests.get(url, headers)
-soup = BeautifulSoup(html.unescape(req.text), "html.parser")
-for e in soup.select("div.mvp-widget-feat2-right a"):
-    result.append(e['href'])
-#    print(e['href'])
-
 def mapper(link):
     return "<a href=\""+link+"\">"+link+"</a></br>"
 
-result = map(mapper, result)
+print("********************")
+#----------manx.news
+def crawl_manx(url, headers):
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(html.unescape(req.text), "html.parser")
+    result = []
+    for e in soup.select("h3.entry-title a"):
+        if "javascript" not in e['href']:
+            result.append(e['href'])
+    result = map(mapper, result)
+    return "</br>".join(result)
+#------------------
+print("********************")
+#---------iomtotday.co.io
+def crawl_iomtoday(url, headers):
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(html.unescape(req.text), "html.parser")
+    result = []
+    for e in soup.select("div div div div div.inner_wrapper div a"):
+        if 'class' not in e and "news" in e['href'] and e['href'][-1] in "0123456789":
+            result.append("https://iomtoday.co.im" + e['href'])
+    result = map(mapper, result)
+    return "</br>".join(result)
+#------------------------
+print("********************")
+#-----------------gef-im
+def crawl_gef(url, headers):
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(html.unescape(req.text), "html.parser")
+    result = []
+    for e in soup.select("div.mvp-widget-feat2-right a"):
+        result.append(e['href'])
+    result = map(mapper, result)
+    return "</br>".join(result)
+#-----------------------
+url = "https://www.manx.news/"
+result = "<div>"+crawl_manx(url, headers)+"</div>"
+url = "https://www.iomtoday.co.im/news"
+result += "<div>"+crawl_iomtoday(url, headers)+"</div>"
+url = "https://gef.im/"
+result += "<div>"+crawl_gef(url, headers)+"</div>"
+result = "<div>"+result+"</div>"
+
 with open("../docs/index.html", "w", encoding="utf-8") as f:
     f.write("".join(result))
